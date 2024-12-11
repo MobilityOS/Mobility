afb087570c04a501e290ac655f0c75f38f17ccfe
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-12 01:08:13 +0800

    Add MoHvCheckAvailability implementation to Mobility.HvGcs.

--- Mobility.HvGcs/Mobility.HvGcs.cpp
+++ Mobility.HvGcs/Mobility.HvGcs.cpp
@@ -13,8 +13,85 @@
 
 #include <Mile.Project.Version.h>
 
+#include <Mile.HyperV.VMBus.h>
+
+#include <intrin.h>
 #include <sal.h>
 #include <string.h>
+
+/**
+ * @brief Checks the Hyper-V guest support availability.
+ * @return If the Hyper-V guest support is available to use, the return value
+ *         is true, or the return value is false.
+ */
+extern "C" bool MoHvCheckAvailability()
+{
+    // Reference: Requirements for Implementing the Microsoft Hypervisor
+    //            Interface
+    //
+    // The minimal interface set required by compliant hypervisors in order to
+    // support Windows operating systems when running in a guest virtual machine
+    // is summarized below. Details of each requirement are provided in
+    // subsequent sections.
+    // - Hypervisor discovery via the CPUID instruction
+    // - Hypervisor CPUID leaves 0x40000000 - 0x40000005
+    // - Hypervisor interface signature equal to "Hv#1"
+    // - Partition privileges AccessVpIndex, AccessHypercallMsrs
+    // - Hypervisor synthetic MSRs HV_X64_MSR_GUEST_OS_ID, HV_X64_MSR_HYPERCALL
+    //   and HV_X64_MSR_VP_INDEX.
+    // - A minimal implementation of the hypercall interface
+    // 
+    // Software determines the presence of a hypervisor through the CPUID
+    // instruction. Processors conforming to the Intel® 64 architecture have
+    // reserved a feature flag in CPUID Function 0x01 - Feature Information for
+    // this purpose.Bit 31 returned in ECX is defined as Not Used, and will
+    // always return 0 from the physical CPU. A hypervisor conformant with the
+    // Microsoft hypervisor interface will set CPUID.1:ECX[bit 31] = 1 to
+    // indicate its presence to software.
+    // 
+    // The Intel® 64 architecture reserves CPUID leaves 0x40000000-0x400000FF
+    // for use by system software. A Microsoft-compliant hypervisor guarantees
+    // leaves 0x40000000 and 0x40000001 are always available.
+    // The hypervisor interface identification is provided at CPUID leaf
+    // 0x40000001. Hypervisors conforming to the Microsoft hypervisor interface
+    // will return the hypervisor interface identification signature 'Hv#1'
+    // (0x31237648) in CPUID.40000001:EAX.   
+
+    HV_CPUID_RESULT HvCpuIdResult;
+
+    ::memset(&HvCpuIdResult, 0, sizeof(HV_CPUID_RESULT));
+    ::__cpuid(
+        reinterpret_cast<int*>(&HvCpuIdResult),
+        HvCpuIdFunctionVersionAndFeatures);
+    if (!HvCpuIdResult.VersionAndFeatures.HypervisorPresent)
+    {
+        return false;
+    }
+
+    ::memset(&HvCpuIdResult, 0, sizeof(HV_CPUID_RESULT));
+    ::__cpuid(
+        reinterpret_cast<int*>(&HvCpuIdResult),
+        HvCpuIdFunctionHvInterface);
+    if (HvMicrosoftHypervisorInterface != HvCpuIdResult.HvInterface.Interface)
+    {
+        return false;
+    }
+
+    ::memset(&HvCpuIdResult, 0, sizeof(HV_CPUID_RESULT));
+    ::__cpuid(
+        reinterpret_cast<int*>(&HvCpuIdResult),
+        HvCpuIdFunctionMsHvFeatures);
+    if (!HvCpuIdResult.MsHvFeatures.PartitionPrivileges.AccessHypercallMsrs)
+    {
+        return false;
+    }
+    if (!HvCpuIdResult.MsHvFeatures.PartitionPrivileges.AccessSynicRegs)
+    {
+        return false;
+    }
+
+    return true;
+}
 
 #define MOBILITY_HVGCS_VERSION_STRING \
     MILE_PROJECT_VERSION_STRING L" (Build " \
@@ -50,6 +127,13 @@ EFI_STATUS EFIAPI UefiMain(
         L" " MOBILITY_HVGCS_VERSION_STRING L"\r\n"
         L"(c) Kenji Mouri. All rights reserved.\r\n"
         L"\r\n");
+
+    if (::MoHvCheckAvailability())
+    {
+        ::OutputWideString(
+            SystemTable->ConOut,
+            L"Yolo!\r\n");
+    }
 
     ::OutputWideString(
         SystemTable->ConOut,
