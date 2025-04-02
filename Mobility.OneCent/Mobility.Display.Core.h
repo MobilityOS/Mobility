ffa6020aa06ff23207cef5bea1757239926be12b
MouriNaruto <Kenji.Mouri@outlook.com>
2025-04-02 08:47:34 +0800

    Implement MO_DISPLAY_BGRA32 series macros for Mobility.Display.Core.

--- Mobility.OneCent/Mobility.Display.Core.h
+++ Mobility.OneCent/Mobility.Display.Core.h
@@ -14,6 +14,17 @@
 #define MILE_MOBILITY_ENABLE_MINIMUM_SAL
 #include <Mile.Mobility.Portable.Types.h>
 
+#define MO_DISPLAY_BGRA32_MAKE(R, G, B, A) ((MO_UINT32)( \
+    (((MO_UINT32)((MO_UINT8)(R))) << 16) | \
+    (((MO_UINT32)((MO_UINT8)(G))) << 8) | \
+    (((MO_UINT32)((MO_UINT8)(B))) << 0) | \
+    (((MO_UINT32)((MO_UINT8)(A))) << 24)))
+
+#define MO_DISPLAY_BGRA32_GET_RED(BGRA32) ((MO_UINT8)((BGRA32) >> 16))
+#define MO_DISPLAY_BGRA32_GET_GREEN(BGRA32) ((MO_UINT8)((BGRA32) >> 8))
+#define MO_DISPLAY_BGRA32_GET_BLUE(BGRA32) ((MO_UINT8)((BGRA32) >> 0))
+#define MO_DISPLAY_BGRA32_GET_ALPHA(BGRA32) ((MO_UINT8)((BGRA32) >> 24))
+
 /**
  * @brief Contains the information of the BGRA32 frame buffer.
  */
