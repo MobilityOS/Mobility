ed6093535a22f10378b2f438eeec722258ebceb7
MouriNaruto <Kenji.Mouri@outlook.com>
2025-04-02 08:40:06 +0800

    Create Mobility.Display.Core.

---
+++ Mobility.OneCent/Mobility.Display.Core.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Display.Core.c
+ * PURPOSE:   Implementation for Mobility Display Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Display.Core.h"
---
+++ Mobility.OneCent/Mobility.Display.Core.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Display.Core.h
+ * PURPOSE:   Definition for Mobility Display Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_DISPLAY_CORE
+#define MOBILITY_DISPLAY_CORE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_DISPLAY_CORE
