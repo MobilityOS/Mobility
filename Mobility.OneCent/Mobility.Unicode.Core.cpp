e08fad1217659ef459ceb518da3d328b31f10c06
MouriNaruto <Kenji.Mouri@outlook.com>
2025-05-05 22:28:22 +0800

    Add Mobility.Unicode.Core.

---
+++ Mobility.OneCent/Mobility.Unicode.Core.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Unicode.Core.c
+ * PURPOSE:   Implementation for Mobility Unicode Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Unicode.Core.h"
---
+++ Mobility.OneCent/Mobility.Unicode.Core.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Unicode.Core.h
+ * PURPOSE:   Definition for Mobility Unicode Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_UNICODE_CORE
+#define MOBILITY_UNICODE_CORE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_UNICODE_CORE
