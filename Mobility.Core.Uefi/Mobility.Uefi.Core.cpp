0f2cd0fd2239c3c44b65a80ba280a1f06d7fa3b6
MouriNaruto <Kenji.Mouri@outlook.com>
2025-05-10 21:36:15 +0800

    Add Mobility.Uefi.Core.

---
+++ Mobility.Core.Uefi/Mobility.Uefi.Core.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Uefi.Core.cpp
+ * PURPOSE:   Implementation for Mobility Uefi Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Uefi.Core.h"
---
+++ Mobility.Core.Uefi/Mobility.Uefi.Core.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Uefi.Core.h
+ * PURPOSE:   Definition for Mobility Uefi Core
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_UEFI_CORE
+#define MOBILITY_UEFI_CORE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_UEFI_CORE
+
