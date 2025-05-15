2ffd03bebfaf7cce75c421e95a318693a4ae8502
MouriNaruto <Kenji.Mouri@outlook.com>
2025-05-16 07:00:49 +0800

    Add Mobility.Otokonoko project.

---
+++ Mobility.Otokonoko/Mobility.Otokonoko.c
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Otokonoko.c
+ * PURPOSE:   Implementation for Mobility Otokonoko
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Otokonoko.h"
+
+EXTERN_C VOID MOAPI MoOtokonokoSimpleFunction()
+{
+
+}
---
+++ Mobility.Otokonoko/Mobility.Otokonoko.h
@@ -0,0 +1,22 @@
+﻿/*
+ * PROJECT:   Mobility
+ * FILE:      Mobility.Otokonoko.h
+ * PURPOSE:   Definition for Mobility Otokonoko
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_OTOKONOKO
+#define MOBILITY_OTOKONOKO
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+ /**
+  * @brief The temporarily function example for noting the API prefix.
+  */
+EXTERN_C VOID MOAPI MoOtokonokoSimpleFunction();
+
+#endif // !MOBILITY_OTOKONOKO
