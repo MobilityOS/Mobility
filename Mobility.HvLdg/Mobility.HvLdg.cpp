7b801215d24a6371ad1b6aba4e768ca578b13632
MouriNaruto <Kenji.Mouri@outlook.com>
2025-11-08 23:13:11 +0800

    Create Mobility.HvLdg project.

---
+++ Mobility.HvLdg/Mobility.HvLdg.cpp
@@ -0,0 +1,61 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.HvLdg.cpp
+ * PURPOSE:    Implementation for Mobility Hyper-V Lightweight Debugger for
+ *             Guests
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#include <Mile.Project.Version.h>
+
+#include <Mobility.Uefi.Core.h>
+
+#define MOBILITY_MINUAP_VERSION_UTF8_STRING \
+    MILE_PROJECT_VERSION_UTF8_STRING " (Build " \
+    MILE_PROJECT_MACRO_TO_UTF8_STRING(MILE_PROJECT_VERSION_BUILD) ")"
+
+/**
+ * Entry point to UEFI Application.
+ *
+ * @param ImageHandle The image handle of the UEFI Application.
+ * @param SystemTable A pointer to the EFI System Table.
+ * @return EFI_STATUS. If the function succeeds, the return value is
+ *         EFI_SUCCESS.
+ */
+EFI_STATUS EFIAPI UefiMain(
+    _In_ EFI_HANDLE ImageHandle,
+    _In_ EFI_SYSTEM_TABLE* SystemTable)
+{
+    ::MoUefiConsoleWriteAsciiString(
+        SystemTable->ConOut,
+        "Mobility Hyper-V Lightweight Debugger for Guests"
+        " " MOBILITY_MINUAP_VERSION_UTF8_STRING "\r\n"
+        "(c) Kenji Mouri. All rights reserved.\r\n"
+        "\r\n");
+
+    ::MoUefiConsoleWriteAsciiString(
+        SystemTable->ConOut,
+        "Hello World!\r\n");
+
+    ::MoUefiConsoleWriteAsciiString(
+        SystemTable->ConOut,
+        "\r\n"
+        "Press any key to return to the boot menu...\r\n");
+    {
+        UINTN Index = 0;
+        SystemTable->BootServices->WaitForEvent(
+            1,
+            &SystemTable->ConIn->WaitForKey,
+            &Index);
+    }
+
+    return EFI_SUCCESS;
+}
+
+#pragma comment(linker, "/entry:UefiMain")
