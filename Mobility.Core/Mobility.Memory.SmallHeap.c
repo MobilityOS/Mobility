291277790d95e93961cb045224379f0be680bde2
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-15 22:39:15 +0800

    Add Mobility.Memory.SmallHeap to Mobility.Core.

---
+++ Mobility.Core/Mobility.Memory.SmallHeap.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.SmallHeap.c
+ * PURPOSE:    Implementation for Mobility Small Memory Heap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Memory.SmallHeap.h"
---
+++ Mobility.Core/Mobility.Memory.SmallHeap.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.SmallHeap.h
+ * PURPOSE:    Definition for Mobility Small Memory Heap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_MEMORY_SMALLHEAP
+#define MOBILITY_MEMORY_SMALLHEAP
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_MEMORY_SMALLHEAP
