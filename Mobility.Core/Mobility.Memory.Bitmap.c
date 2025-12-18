379fa98337f98956843d080f97d4f6a5e1ca9e64
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-18 23:31:10 +0800

    Add Mobility.Memory.Bitmap to Mobility.Core.

---
+++ Mobility.Core/Mobility.Memory.Bitmap.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.Bitmap.c
+ * PURPOSE:    Implementation for Mobility Memory Bitmap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Memory.Bitmap.h"
---
+++ Mobility.Core/Mobility.Memory.Bitmap.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.Bitmap.h
+ * PURPOSE:    Definition for Mobility Memory Bitmap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_MEMORY_BITMAP
+#define MOBILITY_MEMORY_BITMAP
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_MEMORY_BITMAP
