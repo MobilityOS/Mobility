71f6c1f4c49f615a60c376ccb59db952d677b0b4
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-20 19:42:29 +0800

    Add Mobility.Runtime.Core to Mobility.Core.

---
+++ Mobility.Core/Mobility.Runtime.Core.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Runtime.Core.c
+ * PURPOSE:    Implementation for Mobility Runtime Core
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Runtime.Core.h"
---
+++ Mobility.Core/Mobility.Runtime.Core.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Runtime.Core.h
+ * PURPOSE:    Definition for Mobility Runtime Core
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_RUNTIME_CORE
+#define MOBILITY_RUNTIME_CORE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_RUNTIME_CORE
