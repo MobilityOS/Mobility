e5ea00f9e74ad490424407a870b0b4d427119e33
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-21 20:58:31 +0800

    Implement MoUefiAcpiStructureValidate, MoUefiAcpiDescriptionTableValidate, MoUefiAcpiQueryExtendedSystemDescriptionTable and MoUefiAcpiQueryDescriptionTable for Mobility.Uefi.Acpi.

--- Mobility.Core.Uefi/Mobility.Uefi.Acpi.cpp
+++ Mobility.Core.Uefi/Mobility.Uefi.Acpi.cpp
@@ -9,3 +9,182 @@
  */
 
 #include "Mobility.Uefi.Acpi.h"
+
+#include <Mobility.Runtime.Core.h>
+
+#include <Guid/Acpi.h>
+#include <IndustryStandard/Acpi20.h>
+
+EXTERN_C MO_BOOL MoUefiAcpiStructureValidate(
+    _In_ MO_POINTER Structure,
+    _In_ MO_UINTN Size)
+{
+    if (!Structure || !Size)
+    {
+        return MO_FALSE;
+    }
+    MO_UINT8 SumByte = 0;
+    if (MO_RESULT_SUCCESS_OK != ::MoRuntimeCalculateSumByte(
+        &SumByte,
+        Structure,
+        Size))
+    {
+        return MO_FALSE;
+    }
+    if (0 != SumByte)
+    {
+        return MO_FALSE;
+    }
+    return MO_TRUE;
+}
+
+EXTERN_C MO_BOOL MoUefiAcpiDescriptionTableValidate(
+    _In_ MO_POINTER DescriptionTable,
+    _In_ MO_UINT32 ExpectedSignature,
+    _In_ MO_UINT8 MinimumRevision)
+{
+    if (!DescriptionTable)
+    {
+        return MO_FALSE;
+    }
+    EFI_ACPI_DESCRIPTION_HEADER* TableHeader =
+        reinterpret_cast<EFI_ACPI_DESCRIPTION_HEADER*>(DescriptionTable);
+    if (!::MoUefiAcpiStructureValidate(TableHeader, TableHeader->Length))
+    {
+        return MO_FALSE;
+    }
+    if (ExpectedSignature != TableHeader->Signature)
+    {
+        return MO_FALSE;
+    }
+    if (TableHeader->Revision < MinimumRevision)
+    {
+        return MO_FALSE;
+    }
+    return MO_TRUE;
+}
+
+EXTERN_C MO_RESULT MOAPI MoUefiAcpiQueryExtendedSystemDescriptionTable(
+    _Out_ PMO_UINT64 TableAddress,
+    _In_ EFI_SYSTEM_TABLE* SystemTable)
+{
+    if (!TableAddress || !SystemTable)
+    {
+        return MO_RESULT_ERROR_INVALID_PARAMETER;
+    }
+    *TableAddress = 0u;
+
+    for (UINTN i = 0; i < SystemTable->NumberOfTableEntries; ++i)
+    {
+        if (0 != ::MoRuntimeMemoryCompare(
+            &SystemTable->ConfigurationTable[i].VendorGuid,
+            &gEfiAcpiTableGuid,
+            sizeof(EFI_GUID)))
+        {
+            continue;
+        }
+
+        using RootTableType = EFI_ACPI_2_0_ROOT_SYSTEM_DESCRIPTION_POINTER;
+        const MO_UINT64 RootTableSignature = 
+            EFI_ACPI_2_0_ROOT_SYSTEM_DESCRIPTION_POINTER_SIGNATURE;
+        const MO_UINT8 RootTableRevision =
+            EFI_ACPI_2_0_ROOT_SYSTEM_DESCRIPTION_POINTER_REVISION;
+        RootTableType* RootTable = reinterpret_cast<RootTableType*>(
+            SystemTable->ConfigurationTable[i].VendorTable);
+        if (!::MoUefiAcpiStructureValidate(
+            RootTable,
+            OFFSET_OF(RootTableType, Length)))
+        {
+            continue;
+        }
+        if (!::MoUefiAcpiStructureValidate(
+            RootTable,
+            RootTable->Length))
+        {
+            continue;
+        }
+        if (RootTableSignature != RootTable->Signature)
+        {
+            continue;
+        }
+        if (RootTableRevision != RootTable->Revision)
+        {
+            continue;
+        }
+        if (!RootTable->XsdtAddress)
+        {
+            continue;
+        }
+
+        if (!::MoUefiAcpiDescriptionTableValidate(
+            reinterpret_cast<MO_POINTER>(RootTable->XsdtAddress),
+            EFI_ACPI_2_0_EXTENDED_SYSTEM_DESCRIPTION_TABLE_SIGNATURE,
+            EFI_ACPI_2_0_EXTENDED_SYSTEM_DESCRIPTION_TABLE_REVISION))
+        {
+            continue;
+        }
+
+        *TableAddress = RootTable->XsdtAddress;
+
+        // Break the for loop since we found the table.
+        break;
+    }
+
+    if (!*TableAddress)
+    {
+        return MO_RESULT_ERROR_NO_INTERFACE;
+    }
+
+    return MO_RESULT_SUCCESS_OK;
+}
+
+EXTERN_C MO_RESULT MOAPI MoUefiAcpiQueryDescriptionTable(
+    _Out_ PMO_UINT64 TableAddress,
+    _In_ MO_UINT32 ExpectedSignature,
+    _In_ MO_UINT8 MinimumRevision,
+    _In_ MO_UINT64 ExtendedSystemDescriptionTable)
+{
+    if (!TableAddress || !ExtendedSystemDescriptionTable)
+    {
+        return MO_RESULT_ERROR_INVALID_PARAMETER;
+    }
+    *TableAddress = 0u;
+
+    if (!::MoUefiAcpiDescriptionTableValidate(
+        reinterpret_cast<MO_POINTER>(ExtendedSystemDescriptionTable),
+        EFI_ACPI_2_0_EXTENDED_SYSTEM_DESCRIPTION_TABLE_SIGNATURE,
+        EFI_ACPI_2_0_EXTENDED_SYSTEM_DESCRIPTION_TABLE_REVISION))
+    {
+        return MO_RESULT_ERROR_INVALID_POINTER;
+    }
+    EFI_ACPI_DESCRIPTION_HEADER* TableHeader =
+        reinterpret_cast<EFI_ACPI_DESCRIPTION_HEADER*>(
+            ExtendedSystemDescriptionTable);
+
+    PMO_UINT64 TableArray = reinterpret_cast<PMO_UINT64>(&TableHeader[1]);
+    MO_UINTN TableCount = TableHeader->Length;
+    TableCount -= sizeof(EFI_ACPI_DESCRIPTION_HEADER);
+    TableCount /= sizeof(MO_UINT64);
+    for (MO_UINTN i = 0; i < TableCount; ++i)
+    {
+        if (!::MoUefiAcpiDescriptionTableValidate(
+            reinterpret_cast<MO_POINTER>(TableArray[i]),
+            ExpectedSignature,
+            MinimumRevision))
+        {
+            continue;
+        }
+
+        *TableAddress = TableArray[i];
+
+        // Break the for loop since we found the table.
+        break;
+    }
+
+    if (!*TableAddress)
+    {
+        return MO_RESULT_ERROR_NO_INTERFACE;
+    }
+
+    return MO_RESULT_SUCCESS_OK;
+}
--- Mobility.Core.Uefi/Mobility.Uefi.Acpi.h
+++ Mobility.Core.Uefi/Mobility.Uefi.Acpi.h
@@ -17,4 +17,77 @@
 #undef NULL
 #include <Uefi.h>
 
+/**
+ * @brief Validates an ACPI structure by checking its checksum.
+ * @param Structure The pointer to the ACPI structure to be validated.
+ * @param Size The size of the ACPI structure in bytes.
+ * @return If the ACPI structure is valid, the return value is MO_TRUE.
+ *         Otherwise, the return value is MO_FALSE.
+ */
+EXTERN_C MO_BOOL MoUefiAcpiStructureValidate(
+    _In_ MO_POINTER Structure,
+    _In_ MO_UINTN Size);
+
+/**
+ * @brief Validates an ACPI description table by checking its signature,
+ *        revision, and checksum.
+ * @param DescriptionTable The pointer to the ACPI description table to be
+ *                         validated.
+ * @param ExpectedSignature The expected signature of the ACPI description
+ *                          table.
+ * @param MinimumRevision The minimum revision of the ACPI description table.
+ * @return If the ACPI description table is valid, the return value is MO_TRUE.
+ *         Otherwise, the return value is MO_FALSE.
+ */
+EXTERN_C MO_BOOL MoUefiAcpiDescriptionTableValidate(
+    _In_ MO_POINTER DescriptionTable,
+    _In_ MO_UINT32 ExpectedSignature,
+    _In_ MO_UINT8 MinimumRevision);
+
+/**
+ * @brief Acquires the physical address of the Extended System Description Table
+ *        (XSDT) from the UEFI system table.
+ * @param TableAddress The pointer to receive the physical address of the
+ *                     Extended System Description Table (XSDT). If this
+ *                     parameter is nullptr, the function returns
+ *                     MO_RESULT_ERROR_INVALID_PARAMETER.
+ * @param SystemTable The pointer to the UEFI system table. If this parameter is
+ *                    nullptr, the function returns
+ *                    MO_RESULT_ERROR_INVALID_PARAMETER.
+ * @return If the function succeeds, it returns MO_RESULT_SUCCESS_OK. Otherwise,
+ *         it returns an MO_RESULT error code.
+ * @remarks If the Extended System Description Table (XSDT) is not found, the
+ *          function returns MO_RESULT_ERROR_NO_INTERFACE.
+ */
+EXTERN_C MO_RESULT MOAPI MoUefiAcpiQueryExtendedSystemDescriptionTable(
+    _Out_ PMO_UINT64 TableAddress,
+    _In_ EFI_SYSTEM_TABLE* SystemTable);
+
+/**
+ * @brief Acquires the physical address of an ACPI description table from the
+ *        Extended System Description Table (XSDT).
+ * @param TableAddress The pointer to receive the physical address of the ACPI
+ *                     description table. If this parameter is nullptr, the
+ *                     function returns MO_RESULT_ERROR_INVALID_PARAMETER.
+ * @param ExpectedSignature The expected signature of the ACPI description
+ *                          table.
+ * @param MinimumRevision The minimum revision of the ACPI description table.
+ * @param ExtendedSystemDescriptionTable The physical address of the Extended
+ *                                       System Description Table (XSDT). If
+ *                                       this parameter is zero, the function
+ *                                       returns
+ *                                       MO_RESULT_ERROR_INVALID_PARAMETER.
+ * @return If the function succeeds, it returns MO_RESULT_SUCCESS_OK. Otherwise,
+ *         it returns an MO_RESULT error code.
+ * @remarks If the specified ACPI description table is not found, the function
+ *          returns MO_RESULT_ERROR_NO_INTERFACE.
+ *          If the Extended System Description Table (XSDT) is invalid, the
+ *          function returns MO_RESULT_ERROR_INVALID_POINTER.
+ */
+EXTERN_C MO_RESULT MOAPI MoUefiAcpiQueryDescriptionTable(
+    _Out_ PMO_UINT64 TableAddress,
+    _In_ MO_UINT32 ExpectedSignature,
+    _In_ MO_UINT8 MinimumRevision,
+    _In_ MO_UINT64 ExtendedSystemDescriptionTable);
+
 #endif // !MOBILITY_UEFI_ACPI
