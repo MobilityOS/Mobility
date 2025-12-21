39020a2cd7dc4ddac6615262b096a8a166790ff0
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-21 13:05:12 +0800

    Add Mobility.Memory.InternalHeap to Mobility.Core.

---
+++ Mobility.Core/Mobility.Memory.InternalHeap.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.InternalHeap.c
+ * PURPOSE:    Implementation for Mobility Memory Internal Heap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Memory.InternalHeap.h"
---
+++ Mobility.Core/Mobility.Memory.InternalHeap.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Memory.InternalHeap.h
+ * PURPOSE:    Definition for Mobility Memory Internal Heap
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_MEMORY_INTERNALHEAP
+#define MOBILITY_MEMORY_INTERNALHEAP
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_MEMORY_INTERNALHEAP
