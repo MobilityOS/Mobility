99d18d5cc088215f299922331e16ae86c27ae6ca
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-21 14:07:28 +0800

    Add Mobility.Uefi.Acpi to Mobility.Core.Uefi.

---
+++ Mobility.Core.Uefi/Mobility.Uefi.Acpi.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Uefi.Acpi.cpp
+ * PURPOSE:    Implementation for Mobility UEFI ACPI functions
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Uefi.Acpi.h"
---
+++ Mobility.Core.Uefi/Mobility.Uefi.Acpi.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Uefi.Acpi.h
+ * PURPOSE:    Definition for Mobility UEFI ACPI functions
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_UEFI_ACPI
+#define MOBILITY_UEFI_ACPI
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#undef NULL
+#include <Uefi.h>
+
+#endif // !MOBILITY_UEFI_ACPI
