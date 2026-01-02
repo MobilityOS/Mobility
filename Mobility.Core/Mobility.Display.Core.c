feb56b2c64814780034537d0ba14070fb4d50832
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-03 06:57:54 +0800

    Update Mobility.Display.Core.

--- Mobility.Core/Mobility.Display.Core.c
+++ Mobility.Core/Mobility.Display.Core.c
@@ -9,3 +9,32 @@
  */
 
 #include "Mobility.Display.Core.h"
+
+EXTERN_C MO_BOOL MOAPI MoDisplayFrameBufferValidate(
+    _In_ PMO_DISPLAY_BGRA32_FRAMEBUFFER DisplayFrameBuffer)
+{
+    if (!DisplayFrameBuffer || !DisplayFrameBuffer->FrameBufferBase)
+    {
+        // Null pointer is invalid.
+        return MO_FALSE;
+    }
+
+    // Note: HorizontalResolution and VerticalResolution are MO_UINT32,
+    if (!DisplayFrameBuffer->HorizontalResolution ||
+        !DisplayFrameBuffer->VerticalResolution)
+    {
+        // Zero or negative resolution is invalid.
+        return MO_FALSE;
+    }
+
+
+    if (DisplayFrameBuffer->PixelsPerScanLine
+        < DisplayFrameBuffer->HorizontalResolution)
+    {
+        // PixelsPerScanLine must be greater than or equal to
+        // HorizontalResolution.
+        return MO_FALSE;
+    }
+
+    return MO_TRUE;
+}
--- Mobility.Core/Mobility.Display.Core.h
+++ Mobility.Core/Mobility.Display.Core.h
@@ -30,9 +30,45 @@
  */
 typedef struct _MO_DISPLAY_BGRA32_FRAMEBUFFER
 {
+    /**
+     * @brief The base address of the frame buffer.
+     */
     PMO_UINT32 FrameBufferBase;
+    /**
+     * @brief The size of the frame buffer in pixels in the X dimension.
+     */
     MO_UINT32 HorizontalResolution;
+    /**
+     * @brief The size of the frame buffer in pixels in the Y dimension.
+     */
     MO_UINT32 VerticalResolution;
+    /**
+     * @brief The number of pixel elements per video memory line.
+     */
+    MO_UINT32 PixelsPerScanLine;
 } MO_DISPLAY_BGRA32_FRAMEBUFFER, *PMO_DISPLAY_BGRA32_FRAMEBUFFER;
+
+#define MO_DISPLAY_BGRA32_FRAMEBUFFER_GET_PIXEL_OFFSET(FrameBuffer, X, Y) ( \
+    (((MO_UINTN)(Y)) * (FrameBuffer)->PixelsPerScanLine) + (X))
+#define MO_DISPLAY_BGRA32_FRAMEBUFFER_GET_PIXEL_ADDRESS(FrameBuffer, X, Y) ( \
+    (FrameBuffer)->FrameBufferBase \
+    + MO_DISPLAY_BGRA32_FRAMEBUFFER_GET_PIXEL_OFFSET(FrameBuffer, X, Y))
+#define MO_DISPLAY_BGRA32_FRAMEBUFFER_GET_PIXEL_COUNT(FrameBuffer) ( \
+    ((MO_UINTN)(FrameBuffer)->HorizontalResolution) \
+    * (FrameBuffer)->VerticalResolution)
+#define MO_DISPLAY_BGRA32_FRAMEBUFFER_GET_SIZE_IN_BYTES(FrameBuffer) ( \
+    ((MO_UINTN)(FrameBuffer)->PixelsPerScanLine) \
+    * (FrameBuffer)->VerticalResolution \
+    * sizeof(MO_UINT32))
+
+/**
+ * @brief Validates a BGRA32 frame buffer structure.
+ * @param DisplayFrameBuffer The pointer to the BGRA32 frame buffer structure to
+ *                           be validated.
+ * @return If the BGRA32 frame buffer structure is valid, the return value is
+ *         MO_TRUE. Otherwise, the return value is MO_FALSE.
+ */
+EXTERN_C MO_BOOL MOAPI MoDisplayFrameBufferValidate(
+    _In_ PMO_DISPLAY_BGRA32_FRAMEBUFFER DisplayFrameBuffer);
 
 #endif // !MOBILITY_DISPLAY_CORE
