804b03e9bb0ec49240196fa27834bac548f29e62
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-04 22:03:06 +0800

    Add Mobility.Platform.Interface to Mobility.Core.

---
+++ Mobility.Core/Mobility.Platform.Interface.h
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Platform.Interface.h
+ * PURPOSE:    Definition for Mobility Platform Abstraction Layer Interface
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_PLATFORM_INTERFACE
+#define MOBILITY_PLATFORM_INTERFACE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+#endif // !MOBILITY_PLATFORM_INTERFACE
