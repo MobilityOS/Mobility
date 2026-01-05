17e13875d2f7ff04a50d9e2985285bbb63b175f4
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-06 00:43:39 +0800

    Add Mobility.Core.HyperV project.

---
+++ Mobility.Core.HyperV/Mobility.HyperV.Core.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.HyperV.Core.cpp
+ * PURPOSE:    Implementation for Mobility Hyper-V Core functions
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.HyperV.Core.h"
