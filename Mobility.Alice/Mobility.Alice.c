76fede2ded7a76a8666ae9abf56d224f46315922
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-06 00:29:34 +0800

    Create Mobility.Alice project.

---
+++ Mobility.Alice/Mobility.Alice.c
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Alice.c
+ * PURPOSE:    Implementation for Mobility Alice
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mobility.Alice.h"
+
+EXTERN_C VOID MOAPI MoAliceSimpleFunction()
+{
+
+}
---
+++ Mobility.Alice/Mobility.Alice.h
@@ -0,0 +1,22 @@
+﻿/*
+ * PROJECT:    Mobility
+ * FILE:       Mobility.Alice.h
+ * PURPOSE:    Definition for Mobility Alice
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MOBILITY_ALICE
+#define MOBILITY_ALICE
+
+#define MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <Mile.Mobility.Portable.Types.h>
+
+ /**
+  * @brief The temporarily function example for noting the API prefix.
+  */
+EXTERN_C VOID MOAPI MoAliceSimpleFunction();
+
+#endif // !MOBILITY_ALICE
