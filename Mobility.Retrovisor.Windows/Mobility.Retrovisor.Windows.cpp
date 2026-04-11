7996d867ca96f67859f2d5548169acccf2a4c884
MouriNaruto <Kenji.Mouri@outlook.com>
2026-04-12 00:24:25 +0800

    Add Mobility.Retrovisor.Windows project.

---
+++ Mobility.Retrovisor.Windows/Mobility.Retrovisor.Windows.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ * PROJECT:    Mobility.Retrovisor.Windows
+ * FILE:       Mobility.Retrovisor.Windows.cpp
+ * PURPOSE:    Implementation for Mobility.Retrovisor.Windows
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <clocale>
+#include <cstdio>
+#include <cwchar>
+
+int main()
+{
+    // I can't use that because of the limitation in VC-LTL.
+    // std::setlocale(LC_ALL, "zh_CN.UTF-8");
+
+    std::setlocale(LC_ALL, "chs");
+
+    std::wprintf(
+        L"Mobility.Retrovisor.Windows\n"
+        L"================================================================\n"
+        L"Hello World!\n");
+
+    return 0;
+}
