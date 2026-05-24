eaa21a7cc657f86ad3b696b787e7955e35b299cc
MouriNaruto <Kenji.Mouri@outlook.com>
2026-05-24 11:46:49 +0800

    Add Mile.Mobility.Utilities.Memory.Unstaged to Mobility.Core.

---
+++ Mobility.Core/Mile.Mobility.Utilities.Memory.Unstaged.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Mobility.Utilities.Memory.Unstaged.c
+ * PURPOSE:    Implementation for Mobility Unstaged Memory Utilities
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mile.Mobility.Utilities.Memory.Unstaged.h"
---
+++ Mobility.Core/Mile.Mobility.Utilities.Memory.Unstaged.h
@@ -0,0 +1,22 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Mobility.Utilities.Memory.Unstaged.h
+ * PURPOSE:    Definition for Mobility Unstaged Memory Utilities
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MILE_MOBILITY_UTILITIES_MEMORY_UNSTAGED
+#define MILE_MOBILITY_UTILITIES_MEMORY_UNSTAGED
+
+#include <Mile.Mobility.Utilities.FixedInteger.h>
+
+/*
+ * - All functions in this header file are C functions.
+ * - All functions are prefixed with "MoMileMemory".
+ * - Only depends on Mile.Mobility.Utilities.FixedInteger.h.
+ */
+
+#endif // !MILE_MOBILITY_UTILITIES_MEMORY_UNSTAGED
