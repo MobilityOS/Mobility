c9124e436ffb0335d4e30d2ab24faa0b6974db2f
MouriNaruto <Kenji.Mouri@outlook.com>
2026-06-28 21:40:42 +0800

    Add Mile.Mobility.Utilities.MemoryAccess.Unstaged to Mobility.Core.

---
+++ Mobility.Core/Mile.Mobility.Utilities.MemoryAccess.Unstaged.c
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Mobility.Utilities.MemoryAccess.Unstaged.c
+ * PURPOSE:    Implementation for Mobility Unstaged Memory Access Utilities
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "Mile.Mobility.Utilities.MemoryAccess.Unstaged.h"
---
+++ Mobility.Core/Mile.Mobility.Utilities.MemoryAccess.Unstaged.h
@@ -0,0 +1,22 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.Mobility.Utilities.MemoryAccess.Unstaged.h
+ * PURPOSE:    Definition for Mobility Unstaged Memory Access Utilities
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MILE_MOBILITY_UTILITIES_MEMORYACCESS_UNSTAGED
+#define MILE_MOBILITY_UTILITIES_MEMORYACCESS_UNSTAGED
+
+#include "Mile.Mobility.Portable.Types.h"
+
+/*
+ * - All functions in this header file are C functions.
+ * - All functions are prefixed with "MoMile".
+ * - Only depends on Mile.Mobility.Portable.Types.h.
+ */
+
+#endif // !MILE_MOBILITY_UTILITIES_MEMORYACCESS_UNSTAGED
