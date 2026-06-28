861bf2393b09d94c252e6706d65bdc9888280a18
MouriNaruto <Kenji.Mouri@outlook.com>
2026-06-28 23:02:52 +0800

    Implement MoMileCompilerBarrier for Mile.Mobility.Utilities.MemoryAccess.Unstaged.

--- Mobility.Core/Mile.Mobility.Utilities.MemoryAccess.Unstaged.h
+++ Mobility.Core/Mile.Mobility.Utilities.MemoryAccess.Unstaged.h
@@ -19,4 +19,28 @@
  * - Only depends on Mile.Mobility.Portable.Types.h.
  */
 
+#ifdef _MSC_VER
+MO_EXTERN_C MO_VOID _ReadWriteBarrier(MO_VOID);
+#pragma intrinsic(_ReadWriteBarrier)
+#endif /* _MSC_VER */
+
+/**
+ * @brief Limits the compiler optimizations that can reorder memory accesses
+ *        across the point of the call.
+ */
+static MO_FORCEINLINE MO_VOID MoMileCompilerBarrier(MO_VOID)
+{
+#ifdef _MSC_VER /* MSVC */
+#ifdef __cplusplus
+    ::_ReadWriteBarrier();
+#else
+    _ReadWriteBarrier();
+#endif
+#elif defined(__GNUC__) || defined(__clang__) /* GCC and Clang */
+    __asm__ __volatile__("" : : : "memory");
+#else
+#error "[Mile.Mobility.Utilities.MemoryAccess.Unstaged] Unsupported platform."
+#endif
+}
+
 #endif // !MILE_MOBILITY_UTILITIES_MEMORYACCESS_UNSTAGED
